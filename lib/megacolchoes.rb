1881a8e0f119245cd707f223a0a5d926622a6f0f
Nathanael Vieira <59123681+NathanaelV@users.noreply.github.com>
2023-04-27 11:09:45 -0300

    Rubocop

--- lib/megacolchoes.rb
+++ lib/megacolchoes.rb
@@ -4,7 +4,6 @@ require "f1sales_custom/hooks"
 module Megacolchoes
   class Error < StandardError; end
   class F1SalesCustom::Hooks::Lead
-    
     def self.switch_source(lead)
       message = lead.message
       source_name = lead.source ? lead.source.name : ''
